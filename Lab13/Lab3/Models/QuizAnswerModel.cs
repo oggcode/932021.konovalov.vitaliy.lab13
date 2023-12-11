fcfa8f55342d1c90e4568645d1c47cd99ce4eb41
oggcode <144151518+oggcode@users.noreply.github.com>
2023-12-11 23:52:53 +0700

    Add files via upload

---
+++ Lab13/Lab3/Models/QuizAnswerModel.cs
@@ -0,0 +1,9 @@
+﻿using Lab3.Utils;
+
+namespace Lab3.Models;
+
+public class QuizAnswerModel
+{
+  [LessThanOrNull(101, ErrorMessage = "Большое число!")]
+  public int? Answer { get; set; }
+}
---
+++ Lab13/Lab3/Startup.cs
@@ -0,0 +1,24 @@
+namespace Lab3;
+
+public class Startup
+{
+  public void ConfigureServices(IServiceCollection services)
+  {
+    services.AddMvc();
+
+    services.AddDistributedMemoryCache();
+    services.AddSession();
+  }
+
+  public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+  {
+    if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+    app.UseSession();
+
+    app.UseStaticFiles();
+    app.UseRouting();
+
+    app.UseEndpoints(endpoints => { endpoints.MapControllerRoute("default", "{controller=Mockups}/{action=Index}"); });
+  }
+}
